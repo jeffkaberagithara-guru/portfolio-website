b0c6435d2d6d51b21ca26c74f8c8a9248f9031ba
jeffkaberagithara-guru <jeffkaberagithara@gmail.com>
2026-08-14 22:43:56 +0300

    Modified eslint file

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: { react },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -22,9 +24,15 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
-       "no-unused-vars": ["error", { 
-      "varsIgnorePattern": "^React$|^motion$|^useState$|^useEffect$",
-      "argsIgnorePattern": "^_" 
-    }]
+    settings: {
+      react: { version: 'detect' },
     },
+    rules: {
+      'react/jsx-uses-vars': 'error',
+      'no-unused-vars': ['error', {
+        varsIgnorePattern: '^React$',
+        argsIgnorePattern: '^_',
+      }],
+    },
+  },
 ])
